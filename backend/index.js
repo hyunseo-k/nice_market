ab99c80812a9d7307014b8a1aaf30830d85ae29c
hyunseo-k <jelly9999.g.skku.edu>
2023-08-23 17:59:08 +0900

    8/23 상세페이지 총 금액 해야됨

--- backend/index.js
+++ backend/index.js
@@ -26,8 +26,9 @@ db.connect((err) => {
 
 app.post('/signup', (req, res) => {
     const { email, password } = req.body;
-    const query = 'INSERT INTO Users (email, password) VALUES (?, ?)';
-    db.query(query, [email, password], (err, result) => {
+    const query = 'INSERT INTO Users (email, password, cart) VALUES (?, ?, ?)';
+    let valuesToInsert= [email,password,"[]"];
+    db.query(query, valuesToInsert, (err, result) => {
       if (err) {
         console.error('Database query error:', err);
         res.status(500).json({ message: 'Database error' });
@@ -67,6 +68,46 @@ app.post('/dup', (req, res) => {
     });
 });
 
+
+app.post('/:user_id/add', (req, res) => {
+  const { user_id } = req.params;
+  const { product_id, quantity } = req.body;
+
+  // 이 부분에서 카트 정보 업데이트 로직을 작성하세요.
+  // 쿼리를 통해 해당 사용자의 카트 정보를 업데이트하고, 새로운 제품을 추가합니다.
+
+  const updateQuery  = `
+    UPDATE Users
+    SET cart = JSON_ARRAY_APPEND(cart, '$', JSON_OBJECT('product_id', ?, 'quantity', ?))
+    WHERE user_id = ?
+  `;
+
+  db.query(updateQuery, [product_id, quantity, user_id], (updateErr, updateResult) => {
+    // console.error("up2", product_id);
+    console.error("qe", updateResult);
+    if (updateErr) {
+      console.error('Database update query error:', updateErr);
+      res.status(500).json({ message: 'Database error' });
+    } else {
+      const selectQuery = 'SELECT * FROM Users WHERE user_id = ?';
+  
+      db.query(selectQuery, [user_id], (selectErr, selectResult) => {
+        if (selectErr) {
+          console.error('Database select query error:', selectErr);
+          res.status(500).json({ message: 'Database error' });
+        } else {
+          const updatedUser = selectResult[0];
+          console.error("up", updatedUser);
+          res.status(200).json({ message: 'Product added to cart', user: updatedUser });
+        }
+      });
+    }
+  });
+});
+
+
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
