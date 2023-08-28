bf127ca4e3c8ffcc06a93639c476705e2ad3b66d
hyunseo-k <jelly9999.g.skku.edu>
2023-08-28 16:01:23 +0900

    완료

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mysql = require('mysql');
+const { useWindowDimensions } = require('react-native-web');
 
 const app = express();
 const port = 3000; // Choose a port number
-
+var ui;
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -26,8 +27,8 @@ db.connect((err) => {
 
 app.post('/signup', (req, res) => {
     const { email, password } = req.body;
-    const query = 'INSERT INTO Users (email, password, cart) VALUES (?, ?, ?)';
-    let valuesToInsert= [email,password,"[]"];
+    const query = 'INSERT INTO Users (email, password, cart, user_order) VALUES (?, ?, ?, ?)';
+    let valuesToInsert= [email,password,"[]","[]"];
     db.query(query, valuesToInsert, (err, result) => {
       if (err) {
         console.error('Database query error:', err);
@@ -96,13 +97,85 @@ const random = (length = 8) => {
 };
 
 app.post('/done-order', async (req, res) => {
-  const { resultMsg, amount, paidAt } = req.body;
-  console.log("rr", paidAt);
-})
+  const { resultMsg, amount, paidAt, goodsName } = req.body;
+
+  if (resultMsg === "정상 처리되었습니다.") {
+    const user_id = ui; // 사용자 ID
+    const product_name = goodsName; // 상품 이름 --> 추후 조정?
+
+    const order_time = paidAt;
+
+    const updateQuery  = `
+      UPDATE Users
+      SET user_order = JSON_ARRAY_APPEND(user_order, '$', JSON_OBJECT('amount', ?, 'product_name', ?, 'order_time', ?))
+      WHERE user_id = ?
+    `;
+
+    db.query(updateQuery, [amount, product_name, order_time, ui], (updateErr, updateResult) => {
+      // console.error("amount", ui);
+      // console.error("qe", updateResult);
+      if (updateErr) {
+        console.error('Database update query error:', updateErr);
+        res.status(500).json({ message: 'Database error' });
+      } else {
+        console.log('Update result:', updateResult);
+        const selectQuery = 'SELECT * FROM Users WHERE user_id = ?';
+    
+        db.query(selectQuery, [user_id], (selectErr, selectResult) => {
+          if (selectErr) {
+            console.error('Database select query error:', selectErr);
+            res.status(500).json({ message: 'Database error' });
+          } else {
+            const updatedUser = selectResult[0];
+            console.error("up", updatedUser);
+            ////성공하면 장바구니 비우기
+            const updateQuery = 'UPDATE Users SET cart = ? WHERE user_id = ?';
+            const emptyCart = JSON.stringify([]); // 비워진 카트 데이터를 JSON 문자열로 저장
+
+            db.query(updateQuery, [emptyCart, user_id], (updateError, updateResult) => {
+              if (updateError) {
+                console.error('Error updating cart:', updateError);
+                res.status(500).json({ error: 'Internal server error' });
+              } else {
+                res.status(200).json({ message: '결제가 성공적으로 처리되었습니다. 기존 화면으로 돌아가세요.' });
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+});
+
+app.delete('/cart-delete', (req, res) => {
+  try {
+    const { user_id } = req.body;
+    if (!user_id) {
+      return res.status(400).json({ error: 'Missing user_id' });
+    }
+
+    // 데이터베이스에서 해당 사용자의 카트 데이터를 초기화
+    const updateQuery = 'UPDATE Users SET cart = ? WHERE user_id = ?';
+    const emptyCart = JSON.stringify([]); // 비워진 카트 데이터를 JSON 문자열로 저장
+
+    db.query(updateQuery, [emptyCart, user_id], (error, results) => {
+      if (error) {
+        console.error('Error updating cart:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      } else {
+        res.status(200).json({ message: 'Cart deleted successfully' });
+      }
+    });
+  } catch (error) {
+    console.error('Error deleting cart:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 app.post('/make-order', async (req, res) => {
   var id = random();
-  const { totalPrice } = req.body;
+  const { totalPrice, user_id, productName } = req.body;
+  ui = user_id
   try {
     const url = 'https://sandbox-api.nicepay.co.kr/v1/checkout';
     const headers = {
@@ -115,7 +188,7 @@ app.post('/make-order', async (req, res) => {
       "orderId": id,
       "clientId": "S1_ce1bb1ebebc44fe1a3f7cec976c83ea7",
       "amount": totalPrice,
-      "goodsName": "test",
+      "goodsName": productName,
       "returnUrl": "http://localhost:3000/done-order",
       "language": "EN",
       "fakeAuth": "true",
